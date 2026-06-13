cb4972ca977e09297c60431e4b8b9af486dc420b
Sivasakthi <sivasakthi0318@gmail.com>
2026-06-13 20:09:19 +0530

    Fix: use lowercase user.js path for case-sensitive deploy

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
